b86617e96c7972280cff6a94a2504686d2a2e788
nicbuon <nicola.buonavoglia@marconibari.edu.it>
2024-11-06 14:39:26 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+int main()
+ {
+    int i
+    float media,livello,somma
+    i=1
+    somma=0
+    while(i<=7) {
+        cout<<"di quanto si alza l'acqua"<< endl;
+        cin>> livello;
+        if(livello>=10) {
+            cout<<"nel giorno"<< i <<"l'acqua è alta"<<endl;
+        }
+        somma=somma+livello;
+        media=somma/7;
+        i=i+1;
+    }
+    if (media>0) {
+        cout<<"l'acqua è  alta"<<endl;
+    } else {
+        cout << "l'acqua è bassa" << endl;
+    }
+    cout<< "La media dei valori è"<<media<<endl;
 }
+        
+        
 
-//LEGGERE LE ISTRUZIONI NEL FILE README.md
